25e18eeb84a4c8c52351745b3bbe398451f04326
BunRum <61174957+BunRum@users.noreply.github.com>
2023-05-30 00:58:31 -0700

    nothin

--- src/fileserver.go
+++ src/fileserver.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
 var FileServerUrl string
@@ -13,7 +14,6 @@ var FileServer *http.Server
 func logRequests(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if _, err := os.Stat(fmt.Sprintf("assets/%s", r.URL.Path)); err == nil {
-
 		} else {
 			log.Printf("%s %s does not exist", r.Method, r.URL.Path)
 		}
@@ -24,12 +24,8 @@ func logRequests(next http.Handler) http.Handler {
 func StartFileServer() {
 	//srv := &http.Server{Addr: ":8080"}
 	Settings := GetSettings()
-	FileServer := &http.Server{Addr: ":8082", Handler: logRequests(http.FileServer(http.Dir(Settings["AssetPath"].(string))))}
-	// always returns error. ErrServerClosed on graceful close
-	err := FileServer.ListenAndServeTLS("./server.crt", "./server.key")
-	if err != nil {
-		return
-	}
+	FileServer = &http.Server{Addr: ":8082", Handler: logRequests(http.FileServer(http.Dir(Settings["AssetPath"].(string))))}
+	log.Fatal(FileServer.ListenAndServeTLS(filepath.Join(AppDirectory, "./server.crt"), filepath.Join(AppDirectory, "./server.key")))
 }
 func RestartFileServer() {
 	if FileServer != nil {
